da67b844f01df0fb4cb51dabd96d1513ed062552
escmagalhaes <93484761+escmagalhaes@users.noreply.github.com>
2024-01-28 18:36:45 -0300

    Add files via upload

---
+++ Python/Predict C2,C4 vs C3,C5 - All proteins.py
@@ -0,0 +1,222 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+random_state_1 = 15
+random_state_2 = random_state_1 + 1
+random_state_3 = random_state_1 + 2
+random_state_4 = random_state_1 + 3
+
+
+# In[ ]:
+
+
+import shap
+import sklearn
+import itertools
+import pydotplus
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from IPython.display import Image 
+
+from sklearn.tree import export_graphviz
+# from sklearn.externals.six import StringIO
+from six import StringIO
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import IterativeImputer, SimpleImputer
+
+# We'll also import some helper functions that will be useful later on.
+from util import load_data, cindex
+
+
+# In[ ]:
+
+
+X = pd.read_csv('2,4 vs. 3,5 - 411p.csv',index_col=0)
+y_df = pd.read_csv('2,4 vs. 3,5 - Y.csv',index_col=0)
+y = y_df['y']
+X_dev, X_test, y_dev, y_test = train_test_split(X, y, test_size=0.2, random_state=random_state_1)
+
+
+# In[ ]:
+
+
+X_train, X_val, y_train, y_val = train_test_split(X_dev, y_dev, test_size=0.25, random_state=random_state_2)
+
+
+# In[ ]:
+
+
+# X_train_dropped = X_train.dropna(axis='rows')
+# y_train_dropped = y_train.loc[X_train_dropped.index]
+# X_val_dropped = X_val.dropna(axis='rows')
+# y_val_dropped = y_val.loc[X_val_dropped.index]
+
+X_train_dropped = X_train
+y_train_dropped = y_train
+X_val_dropped = X_val
+y_val_dropped = y_val
+
+
+# In[ ]:
+
+
+def holdout_grid_search(clf, X_train_hp, y_train_hp, X_val_hp, y_val_hp, hyperparams, fixed_hyperparams={}):
+    '''
+    Conduct hyperparameter grid search on hold out validation set. Use holdout validation.
+    Hyperparameters are input as a dictionary mapping each hyperparameter name to the
+    range of values they should iterate over. Use the cindex function as your evaluation
+    function.
+
+    Input:
+        clf: sklearn classifier
+        X_train_hp (dataframe): dataframe for training set input variables
+        y_train_hp (dataframe): dataframe for training set targets
+        X_val_hp (dataframe): dataframe for validation set input variables
+        y_val_hp (dataframe): dataframe for validation set targets
+        hyperparams (dict): hyperparameter dictionary mapping hyperparameter
+                            names to range of values for grid search
+        fixed_hyperparams (dict): dictionary of fixed hyperparameters that
+                                  are not included in the grid search
+
+    Output:
+        best_estimator (sklearn classifier): fitted sklearn classifier with best performance on
+                                             validation set
+        best_hyperparams (dict): hyperparameter dictionary mapping hyperparameter
+                                 names to values in best_estimator
+    '''
+    best_estimator = None
+    best_hyperparams = {}
+    
+    # hold best running score
+    best_score = 0.0
+
+    # get list of param values
+    lists = hyperparams.values()
+    
+    # get all param combinations
+    param_combinations = list(itertools.product(*lists))
+    total_param_combinations = len(param_combinations)
+
+    # iterate through param combinations
+    for i, params in enumerate(param_combinations, 1):
+        # fill param dict with params
+        param_dict = {}
+        for param_index, param_name in enumerate(hyperparams):
+            param_dict[param_name] = params[param_index]
+            
+        # create estimator with specified params
+        estimator = clf(**param_dict, **fixed_hyperparams)
+
+        # fit estimator
+        estimator.fit(X_train_hp, y_train_hp)
+        
+        # get predictions on validation set
+        preds = estimator.predict_proba(X_val_hp)
+        
+        # compute cindex for predictions
+        estimator_score = cindex(y_val_hp, preds[:,1])
+
+        print(f'[{i}/{total_param_combinations}] {param_dict}')
+        print(f'Val C-Index: {estimator_score}\n')
+
+        # if new high score, update high score, best estimator
+        # and best params 
+        if estimator_score >= best_score:
+                best_score = estimator_score
+                best_estimator = estimator
+                best_hyperparams = param_dict
+
+    # add fixed hyperparamters to best combination of variable hyperparameters
+    best_hyperparams.update(fixed_hyperparams)
+    
+    return best_estimator, best_hyperparams
+
+
+# In[ ]:
+
+
+def random_forest_grid_search(X_train_dropped, y_train_dropped, X_val_dropped, y_val_dropped):
+
+    # Define ranges for the chosen random forest hyperparameters 
+    hyperparams = {
+        
+        ### START CODE HERE (REPLACE array values with your code) ###
+
+        # how many trees should be in the forest (int)
+        'n_estimators': [50,100,150,200,250,300],
+
+        # the maximum depth of trees in the forest (int)
+        
+        'max_depth': [1,2,3,4,5],
+        
+        # the minimum number of samples in a leaf as a fraction
+        # of the total number of samples in the training set
+        # Can be int (in which case that is the minimum number)
+        # or float (in which case the minimum is that fraction of the
+        # number of training set samples)
+        'min_samples_leaf': [1,2,3,4,5],
+
+        ### END CODE HERE ###
+    }
+
+    
+    fixed_hyperparams = {
+        'random_state': 9,
+    }
+    
+    rf = RandomForestClassifier
+
+    best_rf, best_hyperparams = holdout_grid_search(rf, X_train_dropped, y_train_dropped,
+                                                    X_val_dropped, y_val_dropped, hyperparams,
+                                                    fixed_hyperparams)
+
+    print(f"Best hyperparameters:\n{best_hyperparams}")
+
+    
+    y_train_best = best_rf.predict_proba(X_train_dropped)[:, 1]
+    print(f"Train C-Index: {cindex(y_train_dropped, y_train_best)}")
+
+    y_val_best = best_rf.predict_proba(X_val_dropped)[:, 1]
+    print(f"Val C-Index: {cindex(y_val_dropped, y_val_best)}")
+    
+    # add fixed hyperparamters to best combination of variable hyperparameters
+    best_hyperparams.update(fixed_hyperparams)
+    
+    return best_rf, best_hyperparams
+
+
+# In[ ]:
+
+
+best_rf, best_hyperparams = random_forest_grid_search(X_train_dropped, y_train_dropped, X_val_dropped, y_val_dropped)
+
+
+# In[ ]:
+
+
+# UNQ_C3 (UNIQUE CELL IDENTIFIER, DO NOT EDIT)
+y_test_best = best_rf.predict_proba(X_test)[:, 1]
+
+print(f"Test C-Index: {cindex(y_test.values, y_test_best)}")
+
+
+# In[ ]:
+
+
+shap_values = shap.TreeExplainer(best_rf).shap_values(X_test)[1]
+
+
+# In[ ]:
+
+
+shap.summary_plot(shap_values, X_test)
+
